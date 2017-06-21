44e792228ced2e5fdb25beeee45cd9e93e039b6a
sunzg <gpfq@qq.com>
2017-06-21 09:31:39 +0800

    8080-80

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get('/',function(req,res){
     var fileName = "public/www.dztchu.com/index.htm";
     res.sendfile(fileName);
 })
-app.listen(8080, () => {
+app.listen(80, () => {
     console.log(`App listening at port 8080`)
 })
